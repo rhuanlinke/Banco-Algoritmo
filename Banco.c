ff9797410804645c3bd77537a6f8012f8b03e9a9
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-04-29 20:48:28 -0300

    While
    Adicionei a repetição While, para que funcione até digitarem a opção 4.
    Além disto adicionei a função de "opção incorreta", e mudei o local do texto das opções para dentro do while.

--- Banco.c
+++ Banco.c
@@ -2,45 +2,52 @@
 
 int main(){
 	
-	float opcao, saldo, saque, deposito;
+	float opcao, saldo = 0.00, saque, deposito;
 	
-	printf("-------------------------\n");
-	printf("Bem vindo ao nosso banco!\n\nSelecione uma das opcoes:\n1 - Saldo\n2 - Saque\n3 - Deposito\n4 - Sair\n");
-	printf("-------------------------\n");
+	printf("------------------------\n");
+	printf("Bem vindo ao nosso banco!\n");
+	printf("------------------------\n");
 	
-	saldo = 2000.00;
-	
-	scanf("%f",&opcao);
-	
-	if(opcao == 1){
-	    printf ("Seu saldo e: %f\n",saldo);
-	    system("pause");
+	while(opcao != 4){
+	    printf ("\nSelecione uma das opcoes:\n1 - Saldo\n2 - Saque\n3 - Deposito\n4 - Sair\n");
+	    scanf("%f",&opcao);
+        if(opcao == 1){
+            printf ("Seu saldo e: %f\n",saldo);
+            
+        }
+        else if(opcao == 2){
+            printf ("Qual o valor do saque?\n");
+            scanf ("%f",&saque);
+            if(saque > saldo){
+                printf ("Saldo insuficiente.\n");
+                
+            }
+            else{
+                saldo = saldo - saque;
+                printf ("Seu saldo e: %f\n",saldo);
+                
+            }
+            
+        }
+        else if(opcao == 3){
+            printf ("Qual o valor do deposito?\n");
+            scanf ("%f",&deposito);
+            if(deposito < 0){
+                printf ("Valor de deposito negativo invalido.\n");
+                
+            }
+            else{
+                saldo = (deposito + saldo);
+                
+            }
+            printf ("Seu saldo e: %f\n",saldo);
+            
+        }
+        
+        else{
+            printf("Opcao Invalida\n");
+        }
+	    
 	}
-	else if(opcao == 2){
-	    printf ("Qual o valor do saque?\n");
-	    scanf ("%f",&saque);
-	    if(saque > saldo){
-	        printf ("Saldo insuficiente.\n");
-	    }
-	    else{
-	        saldo = (saldo-saque);
-	        printf ("Seu saldo e: %f\n",saldo);    
-	    }
-	    system("pause");
-	}
-	else if(opcao == 3){
-	    printf ("Qual o valor do deposito?\n");
-	    scanf ("%f",&deposito);
-	    if(deposito < 0){
-	        printf ("Valor de deposito negativo invalido.\n");
-	    }
-	    else{
-	        saldo = (deposito + saldo);
-	    }
-	    printf ("Seu saldo e: %f\n",saldo);
-	    system("pause");
-	}
-	else if(opcao == 4){
-	    system("exit");
-	}
+    
 }
