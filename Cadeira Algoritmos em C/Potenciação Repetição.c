e49b00aca32ce5c2616906f03f803ca4accd9575
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-05-09 16:07:06 -0300

    Correção de bug
    Não estava potenciação, somente multiplicando o primeiro número pelo segundo.
    
    Criado um cont4, que servirá como auxiliar que irá resetar toda vez que q base for somada o numero de vezes que ela representa. Isso acontecerá o número da potenciação em vezes.
    
    5² por exemplo:
    cont4 = cont4 + basealt(5)
    isso acontecerá 5 vezes, resultando em 25.
    Após isto a basealt se tornará o resultado da cont4 (25), e passará para a segunda potencia...
    cont4 = cont4 + basealt(25), isso acontecerá 5 vezes, resultando em 125.

--- Cadeira Algoritmos em C/Potenciação Repetição.c
+++ Cadeira Algoritmos em C/Potenciação Repetição.c
@@ -1,3 +1,4 @@
+
 /******************************************************************************
 1. Recebe 2 valores inteiros (base e expoente), positivos e maiores que zero 
 2. calcule a potencia desses valores sem utilizar funções e o sinal de multiplicação (*).
@@ -9,7 +10,7 @@
 int main()
 {
     
-    int basefix, basealt, expoente, cont = 0, cont2 = 1;
+    int basefix, expoente, cont, cont2 = 1, cont3, basealt, cont4;
     
     printf("Digite a Base e o Expoente. Ambos devem ser inteiros, positivos e maiores que zero.\n");
     scanf("%d %d",&basefix,&expoente);
@@ -26,10 +27,16 @@ int main()
         else{
             basealt = basefix;
             cont = 1;
-            while(cont2 < expoente){
-                basealt = basealt + basefix;
+                while(cont2 < expoente){
                 cont2++;
-            }
+                cont4 = 0;
+                cont3 = 1;
+                    while(cont3 <= basefix){
+                    cont4 = cont4 + basealt;
+                    cont3++;
+                    }
+                basealt = cont4;
+                }
         }   
     }
     printf("Resultado: %d",basealt);
