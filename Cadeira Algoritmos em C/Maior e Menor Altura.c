fe8597aa1634c0c3e14678dce42351dc89103863
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-05-27 22:45:54 -0300

    Vetores
    Vetores adicionados

--- Cadeira Algoritmos em C/Maior e Menor Altura.c
+++ Cadeira Algoritmos em C/Maior e Menor Altura.c
@@ -1,42 +1,46 @@
-/******************************************************************************
+/*2. Escreva um algoritmo que armazene em vetores 1000 informações contendo, 
+cada uma delas, a altura e o sexo de uma pessoa 
+(código=1, masculino  e código=2, feminino), calcule e mostre o seguinte:
 
-2.Escrever um algoritmo que leia um conjunto de 50000 informações contendo, cada uma delas, a altura e o sexo de uma pessoa 
-(código=1, masculino  e código=2, feminino), calcule e mostre o seguinte: 
+- A maior e a menor altura da turma.
+- A média da altura das mulheres.
+- A média da altura da turma. */
 
-a) a maior e a menor altura da turma
-b) a média da altura das mulheres
-c) a média da altura da turma
-
-*******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-    int contador = 1, contadorm = 1, sexo;
-    float altura, alturam = 0.00, alturat = 0.00, menoraltura, maioraltura;
 
-    printf("Calculo de Alturas da Turma\n\n");
-    
-    while(contador <= 50000){
-        printf("Qual altura do aluno?\n");
-        scanf("%f",&altura);
-        printf("Qual o sexo do aluno?\n1. Masculino\n2. Feminino\n");
-        scanf("%d",&sexo);
-        contador = contador + 1;
-        alturat = alturat + altura;
-        if (sexo = 2){
-            alturam = alturam + altura;
-            contadorm = contadorm + 1;
+    float altura[4], maior_altura, menor_altura, media_mulher = 0, media_turma = 0;
+    int sexo[4], cont = 0, cont2 = 0;
+
+    while (cont < 4)
+    {
+        printf("Qual sua altura?\n");
+        scanf("%f", &altura[cont]);
+        maior_altura = altura[cont];
+        menor_altura = altura[cont];
+        printf("Qual seu sexo?\n1 =M\n2 =F\n");
+        scanf("%d", &sexo[cont]);
+        if (sexo[cont] == 2)
+        {
+            media_mulher = altura[cont] + media_mulher;
+            cont2++;
         }
-        if (altura > maioraltura){
-            maioraltura = altura;
+        if (altura[cont] > maior_altura)
+        {
+            maior_altura = altura[cont];
         }
-        if (altura < menoraltura || altura > 0.00){
-            menoraltura = altura;
+        else if (altura[cont] < menor_altura)
+        {
+            menor_altura = altura[cont];
         }
+        media_turma = altura[cont] + media_turma;
+        cont++;
+        printf("\n");
     }
-    printf("\nA maior e a menor altura da turma, respectivamente sao:\nMaior: %f\nMenor: %f",maioraltura,menoraltura);
-    printf("\nMedia da altura das mulheres: %f",alturam/contador);
-    printf("\nMedia da altura da turma: %f",alturat/contadorm);
-
+    media_mulher = media_mulher / cont2;
+    media_turma = media_turma / cont;
+    printf("Maior altura: %f\nMenor altura: %f\n", maior_altura, menor_altura);
+    printf("Media da altura das mulheres: %f\nMedia da altura da turma: %f", media_mulher, media_turma);
 }
--- Cadeira Algoritmos em C/Numeros Primos.c
+++ Cadeira Algoritmos em C/Numeros Primos.c
@@ -1,33 +1,30 @@
 #include<stdio.h>
 
-/*Faça um algoritmo que recebe somente numeros inteiros, positivos e maiores que zero. O algoritmo deve verificar se o número informado é primo ou não.*/
+/*4.  Escrever um algoritmo que recebe 1000 numeros e armazena em um Vetor somente os numeros primos.*/
 
 int main(){
 	
-    int nro, nro2, cont = 0, cont2 = 0, cont3 = 0;
+    int nro, nro2, cont = 0, cont2 = 0, cont3 = 0, primos[1000];
     printf("Digite um numero inteiro e positivo:\n");
     scanf("%d",&nro);
     
-    while(cont == 0){
-        if(nro < 0){
-            printf("Numero negativo invalido. Digite um numero inteiro positivo:\n");
-            scanf("%d",&nro);
-        }
-        else{
-            cont = 1;
-            while(cont2 < nro){
-                cont2 = cont2 + 1;
-                if(nro % cont2 == 0){
-                 cont3 = cont3 + 1;
+    while(cont3 < 1000){
+        printf("Digite um numero inteiro e positivo:\n");
+        scanf("%d",&nro);
+            if(nro < 0){
+                printf("Numero negativo invalido. Digite um numero inteiro positivo:\n");
+                scanf("%d",&nro);
+            }
+            else{
+                cont3++;
+                while(cont2 < nro){
+                    cont2 ++;
+                    if(nro % cont2 == 0){
+                    primos[cont3] = nro;
+                    
                 }
             }
         }
-    }
-    if(cont3 == 2){
-        printf("\nO numero %d eh primo.",nro);
-    }
-    else{
-        printf("\nO numero %d nao eh primo.",nro);
     }
     
 }
