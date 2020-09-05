72d76c08afd9472cd9ad2ff22eba3e2e3fa04034
Rhuan Santos Linke <61024134+rhuanlinke@users.noreply.github.com>
2020-09-05 19:03:26 -0300

    Update ponteiros1.c
    //& devolve o endereço na memória do seu operando.

--- Cadeira Estrutura de Dados/ponteiros1.c
+++ Cadeira Estrutura de Dados/ponteiros1.c
@@ -4,7 +4,7 @@
 int main (int argc, char *argv[])
 {
     int *ponteiro1[5], *ponteiro2, endereco = 1, valor, i;
-
+    int *o, p, *j, *k;
     //valor = ponteiro1;
 
     for(i = 0;i < 5;i++)
@@ -12,5 +12,12 @@ int main (int argc, char *argv[])
     printf("\n%p",ponteiro1[i]); //%p ele trará o endereço do ponteiro
     }
     //printf("\n%p",valor);
-
+    p = 10;
+    o = &p; //& devolve o endereço na memória do seu operando. Dentro de "o" ficará o endereço de p.
+    j = &p;
+    valor = *o; //*o tem um valor dentro de si
+    printf("\n\n\n%d",valor);
+    printf("\n\n\n%d",o);
+    printf("\n\n\n%p",o);
+    printf("\n\n\n%d",j);
 }
